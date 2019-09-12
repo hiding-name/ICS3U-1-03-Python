7fedf4c107510a09677bd9abb14b98e15dbf6e77
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-09-12 18:40:00 +0000

    fancy extra

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ICS3U-1-03-FancyPython
+
+This is the one program he had is write stuff around it. Kinda blerrler but okay.
---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Matsuru Hoshi
+# Created on: Sept 2019
+# This is a "Hello, World!" with extra stuff around it
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
